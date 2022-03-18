e3d77e5772d660db0f402c9ad1e09c401910f5e0
kishanmanoharan <45130704+kishanmanoharan@users.noreply.github.com>
2022-03-18 18:09:22 -0400

    Remove unused library

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col, Button, Alert } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export const Home = () => {
